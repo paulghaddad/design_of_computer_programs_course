4c47657f89a6fb6e536d05a5f1c9570f25b1298e
Paul Haddad <paulh16@gmail.com>
2018-09-20 07:15:57 -0400

    Through Lesson 1 - Video 16
    * Implement `hand_rank`.
    * Add tests for `hand_rank` and `card_ranks`.

--- lessons/poker/poker.py
+++ lessons/poker/poker.py
@@ -4,4 +4,27 @@ def poker(hands):
 
 def hand_rank(hand):
     "Return a value indicating the ranking of a hand."
-
+    ranks = card_ranks(hand)
+    if straight(ranks) and flush(hand):
+        # We only need the high card to break ties
+        # A A A A 4 => (8, 14)
+        return (8, max(ranks))
+    elif kind(4, ranks):
+        # kind must return the rank of the kind
+        # 9 9 9 9 3 => (7, 9, 3)
+        return (7, kind(4, ranks), kind(1, ranks))
+    elif kind(3, ranks) and kind(2, ranks):
+        return (6, kind(3, ranks), kind(2, ranks))
+    elif flush(hand):
+        return (5, ranks)
+    elif straight(ranks):
+        return (4, max(ranks))
+    elif kind(3, ranks):
+        return (3, kind(3, ranks), ranks)
+    elif two_pair(ranks):
+        pairs = two_pair(ranks)
+        return (2, two_pair(ranks), ranks)
+    elif kind(2, ranks):
+        return (1, kind(2, ranks), ranks)
+    else:
+        return (0, ranks)
--- lessons/poker/test_poker.py
+++ lessons/poker/test_poker.py
@@ -13,3 +13,38 @@ def test_poker():
     # Test extreme values
     assert poker([fh]) == fh
     assert poker([sf] + 99*[fh]) == sf
+
+
+def test_hand_rank():
+    sf = "6C 7C 8C 9C TC".split()
+    fk = "9D 9H 9S 9C 7D".split()
+    fh = "TD TC TH 7C 7D".split()
+    flush = "10H 8H 7H 5H 3H".split()
+    straight = "JD 10S 9C 8D 7H".split()
+    three_kind = "7C 7D 7H 5H 2S".split()
+    two_pair = "JC JD 3H 3S KS".split()
+    pair = "2S 2C JC 6S 3H".split()
+    high_card = "7H 5S 4D 3C 2H".split()
+
+    assert hand_rank(sf) == (8, 10)
+    assert hand_rank(fk) == (7, 9, 7)
+    assert hand_rank(fh) == (6, 10, 7)
+    assert hand_rank(flush) == (5, [10, 8, 7, 5, 3])
+    assert hand_rank(straight) == (4, 11)
+    assert hand_rank(three_kind) == (3, 7, [7, 7, 7, 5, 2])
+    assert hand_rank(two_pair) == (2, 11, 3, [13, 11, 11, 3, 3])
+    assert hand_rank(pair) == (1, 2, [11, 6, 3, 2, 2])
+    assert hand_rank(high_card) == (0, [7, 5, 4, 3, 2])
+
+def test_card_ranks():
+    """
+    card_ranks returns the ranks in sorted order
+    """
+
+    sf = "6C 7C 8C 9C TC".split()
+    fk = "9D 9H 9S 9C 7D".split()
+    fh = "TD TC TH 7C 7D".split()
+
+    assert card_ranks(sf) == [10, 9, 8, 7, 6]
+    assert card_ranks(fk) == [9, 9, 9, 9, 7]
+    assert card_ranks(fh) == [10, 10, 10, 7, 7]
