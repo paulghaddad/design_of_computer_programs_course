92d438c71ed3365660d99c6f27e82a20fb835f42
Paul Haddad <paulh16@gmail.com>
2018-10-03 07:14:05 -0400

    Refactor seven card stud solution:
    * Rename variable
    * Only need to return the single best hand, not the list, so `hand_rank`
    is the better function to use, and simplifies the code greatly.

--- lessons/poker/seven_card_stud.py
+++ lessons/poker/seven_card_stud.py
@@ -1,10 +1,9 @@
 import itertools
-from poker import allmax, hand_rank
+from poker import hand_rank
 
 
 def best_hand(hand, n=5):
     """From a 7-card hand, return the best 5 card hand."""
-    five_card_combinations = itertools.combinations(hand, n)
+    combinations = itertools.combinations(hand, n)
 
-    best_hands = [list(hand) for hand in allmax(five_card_combinations, key=hand_rank)]
-    return best_hands[0]
+    return max(combinations, key=hand_rank)
