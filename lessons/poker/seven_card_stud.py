85ce9ebd18045c3af4410dd92a00dd9e14905582
Paul Haddad <paulh16@gmail.com>
2018-10-03 07:05:58 -0400

    Get all tests to pass for seven_card_stud; now to refactor

--- lessons/poker/seven_card_stud.py
+++ lessons/poker/seven_card_stud.py
@@ -1,2 +1,10 @@
-def best_hand():
-    pass
+import itertools
+from poker import allmax, hand_rank
+
+
+def best_hand(hand, n=5):
+    """From a 7-card hand, return the best 5 card hand."""
+    five_card_combinations = itertools.combinations(hand, n)
+
+    best_hands = [list(hand) for hand in allmax(five_card_combinations, key=hand_rank)]
+    return best_hands[0]
