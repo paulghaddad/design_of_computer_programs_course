89be40c03fbb4f472d75d35e650b15e658cd8f91
Paul Haddad <paulh16@gmail.com>
2018-10-09 07:56:32 -0400

    Zebra Problem
    * Refactor to use a generator

--- lessons/zebra.py
+++ lessons/zebra.py
@@ -16,25 +16,26 @@ def zebra_puzzle():
     houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
     orderings = list(itertools.permutations(houses)) # Constraint 1
 
-    for (red, green, ivory, yellow, blue) in orderings:
-        for (Englishman, Spaniard, Ukranian, Japanese, Norweigian) in orderings:
-            for (dog, snails, fox, horse, ZEBRA) in orderings:
-                for (coffee, tea, milk, oj, WATER) in orderings:
-                    for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings:
-                        if Englishman is red:  # Constraint 2
-                            if Spaniard is dog: #3
-                                if coffee is green: #4
-                                    if Ukranian is tea: #5
-                                        if imright(green, ivory): #6
-                                            if OldGold is snails: #7
-                                                if Kools is yellow: #8
-                                                    if milk is middle: #9
-                                                        if Norweigian is first: #10
-                                                            if nextto(Chesterfields, fox): #11
-                                                                if nextto(Kools, horse): #12
-                                                                    if LuckyStrike is oj: #13
-                                                                        if Japanese is Parliaments: #14
-                                                                            if nextto(Norweigian, blue): #15
-                                                                                return (WATER, ZEBRA)
+    return next((WATER, ZEBRA) 
+            for (red, green, ivory, yellow, blue) in orderings
+            for (Englishman, Spaniard, Ukranian, Japanese, Norweigian) in orderings
+            for (dog, snails, fox, horse, ZEBRA) in orderings
+            for (coffee, tea, milk, oj, WATER) in orderings
+            for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
+            if Englishman is red  # Constraint 2
+            if Spaniard is dog #3
+            if coffee is green #4
+            if Ukranian is tea #5
+            if imright(green, ivory) #6
+            if OldGold is snails #7
+            if Kools is yellow #8
+            if milk is middle #9
+            if Norweigian is first #10
+            if nextto(Chesterfields, fox) #11
+            if nextto(Kools, horse) #12
+            if LuckyStrike is oj #13
+            if Japanese is Parliaments #14
+            if nextto(Norweigian, blue) #15
+            )
 
 zebra_puzzle()
