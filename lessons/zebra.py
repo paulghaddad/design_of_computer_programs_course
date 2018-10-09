bf3f61cd2c5f941aef6fad24086a9652d6ca8c29
Paul Haddad <paulh16@gmail.com>
2018-10-09 07:31:37 -0400

    Zebra Problem
    * Add two additional functions to determine if a house is immediately to
    the right of another house, as well as whether it's next to another
    house.

--- lessons/zebra.py
+++ lessons/zebra.py
@@ -3,10 +3,21 @@ import itertools
 houses = [1, 2, 3, 4, 5]
 orderings = itertools.permutations(houses)
 
+
+def imright(h1, h2):
+    "House h1 is immediately right of h2 if h1-h2 = 1"
+    return h1 - h2 == 1
+
+
+def nextto(h1, h2):
+    "Two houses are next to each other if they differ by 1"
+    return abs(h1 - h2) == 1
+
+
 for (red, green, ivory, yellow, blue) in orderings:
     for (Englishman, Spaniard, Ukranian, Japanese, Norweigian) in orderings:
         for (dog, snails, fox, horse, ZEBRA) in orderings:
             for (coffee, tea, milk, oj, WATER) in orderings:
                 for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings:
                     # Constraint #2: The Englishman lives in the red house
-                    if (red == Englishman): # Check to see if red and Englishman have been assigned the same house number
+                    if (red == Englishman): 
