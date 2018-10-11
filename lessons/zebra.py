e5899528241dbdf740bab59b7bc018c097812c3c
Paul Haddad <paulh16@gmail.com>
2018-10-11 08:01:03 -0400

    Zebra Puzzle
    * Add debugging code to understand the number of for loops we enter and
    the number of items we iterate through.
    * This is code for the debugging aspect of the program.

--- lessons/zebra.py
+++ lessons/zebra.py
@@ -1,4 +1,5 @@
 import itertools
+import time
 
 
 def imright(h1, h2):
@@ -17,28 +18,37 @@ def zebra_puzzle():
     orderings = list(itertools.permutations(houses)) # Constraint 1
 
     return next((WATER, ZEBRA) 
-            for (red, green, ivory, yellow, blue) in orderings
+            for (red, green, ivory, yellow, blue) in c(orderings)
             if imright(green, ivory) #6
-            for (Englishman, Spaniard, Ukranian, Japanese, Norweigian) in orderings
+            for (Englishman, Spaniard, Ukranian, Japanese, Norweigian) in c(orderings)
             if Englishman is red  # 2
             if Norweigian is first #10
             if nextto(Norweigian, blue) #15
-            for (coffee, tea, milk, oj, WATER) in orderings
+            for (coffee, tea, milk, oj, WATER) in c(orderings)
             if coffee is green #4
             if Ukranian is tea #5
             if milk is middle #9
-            for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
+            for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in c(orderings)
             if Kools is yellow #8
             if LuckyStrike is oj #13
             if Japanese is Parliaments #14
-            for (dog, snails, fox, horse, ZEBRA) in orderings
+            for (dog, snails, fox, horse, ZEBRA) in c(orderings)
             if Spaniard is dog #3
             if OldGold is snails #7
             if nextto(Chesterfields, fox) #11
             if nextto(Kools, horse) #12
             )
 
-import time
+
+def instrument_fn(fn, *args):
+    c.starts, c.items = 0, 0
+    result = fn(*args)
+    print(f"{fn.__name__} got {result} with {c.starts} iters over {c.items} items")
+
+
+# >>> instrument_fn(zebra_puzzle)
+# zebra_puzzle got (1, 5) with 25 iters over 2775 items.
+
 
 def timedcall(fn, *args):
     "Call function with args; return the time in seconds and result."
