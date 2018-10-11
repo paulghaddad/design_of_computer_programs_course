f644ffcae1ccac0da76e27d0a056a70b939f38b9
Paul Haddad <paulh16@gmail.com>
2018-10-11 07:26:20 -0400

    Zebra Puzzle
    * Add functions to perform repeated measurements of the timing of the
    zebra_puzzle function.

--- lessons/zebra.py
+++ lessons/zebra.py
@@ -40,10 +40,23 @@ def zebra_puzzle():
 
 import time
 
-def timedcall(fn):
+def timedcall(fn, *args):
+    "Call function with args; return the time in seconds and result."
     t0 = time.perf_counter()
-    fn()
+    result = fn(*args)
     t1 = time.perf_counter()
-    return t1-t0
+    return t1-t0, result
 
-print(timedcall(zebra_puzzle))
+
+def timedcalls(n, fn, *args):
+    "Call function n times with args; return the min, avg and max time."
+    times = [timedcall(fn, *args)[0] for _ in range(n)]
+    return min(times), average(times), max(times)
+
+
+def average(numbers):
+    "Return the average (arithmetic mean) of a sequence of numbers."
+    return sum(numbers) / float(len(numbers))
+
+
+print(timedcalls(1000, zebra_puzzle))
