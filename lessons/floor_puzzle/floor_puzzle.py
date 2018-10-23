ecfc1662c387d8edb1f7accb58a6afcd8d74acad
Paul Haddad <paulh16@gmail.com>
2018-10-23 07:39:25 -0400

    Floor Puzzle
    * Get the implementation to work

--- lessons/floor_puzzle/floor_puzzle.py
+++ lessons/floor_puzzle/floor_puzzle.py
@@ -11,4 +11,26 @@ def determine_floor_assignments():
     floor_assignment_permutations = permutations(floors, 5)
 
     for hopper, kay, liskov, perlis, ritchie in floor_assignment_permutations:
-        print(hopper, kay, liskov, perlis, ritchie)
+        if hopper != 5:
+            if kay != 1:
+                if liskov not in (1, 5):
+                    if higher_floor(perlis, kay):
+                        if not adjacent_floor(ritchie, liskov):
+                            if not adjacent_floor(liskov, kay):
+                                return hopper, kay, liskov, perlis, ritchie
+
+
+def higher_floor(person_1, person_2):
+    """
+    Returns True if person_1 lives on a higher floor than person_2. Otherwise,
+    returns False.
+    """
+    return person_1 > person_2
+
+
+def adjacent_floor(person_1, person_2):
+    """
+    Returns True if person_1 lives on a floor immediately above or below
+    person_2.
+    """
+    return abs(person_1 - person_2) == 1
