80b32a377a8f3ee1e4b7cd9159d73cbf664fde0f
Paul Haddad <paulh16@gmail.com>
2018-10-24 08:14:17 -0400

    Longest Subpalindrome
    * Get it working from left to right.

--- lessons/subpalindrome/subpalindrome.py
+++ lessons/subpalindrome/subpalindrome.py
@@ -1,8 +1,18 @@
 def longest_subpalindrome_slice(string):
-    if _palindrome(string.lower()):
-        return (0, len(string))
-    else:
-        return longest_subpalindrome_slice(string[:-1])
+    normalized_string = string.lower()
+
+    longest_subpalindrome = (0,0)
+    for i, starting_letter in enumerate(normalized_string):
+        for x, ending_letter in enumerate(normalized_string[i:]):
+            substring = normalized_string[i:i+x+1]
+            print(substring)
+
+            if _palindrome(substring):
+                if len(substring) > (longest_subpalindrome[1] - longest_subpalindrome[0]):
+                    print(f"Longest Palindrom! {substring}")
+                    longest_subpalindrome = (i, i+x+1)
+
+    return longest_subpalindrome
 
 
 def _palindrome(string):
--- lessons/subpalindrome/test_subpalindrome.py
+++ lessons/subpalindrome/test_subpalindrome.py
@@ -6,11 +6,10 @@ from subpalindrome import longest_subpalindrome_slice
 def test_longest_subpalindrome_slice_for_palindromes():
     L = longest_subpalindrome_slice
 
-    assert L('racecar') == (0, 7)
-    # assert L('ab') is None
-    assert L('Racecar') == (0, 7)
-    assert L('') == (0, 0)
-    assert L('xxxxx') == (0, 5)
+    # assert L('racecar') == (0, 7)
+    # assert L('Racecar') == (0, 7)
+    # assert L('') == (0, 0)
+    # assert L('xxxxx') == (0, 5)
 
 
 def test_longest_subpalindrome_slice_for_subpalindromes():
@@ -19,3 +18,4 @@ def test_longest_subpalindrome_slice_for_subpalindromes():
     assert L('RacecarX') == (0, 7)
     assert L('xxxabc') == (0, 3)
     assert L('Mad am I ma dam.') == (0, 15)
+    assert L('Race carr') == (7, 9)
