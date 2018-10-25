a30af6cec82266af7d3c1dd6f54e9a43b09dc186
Paul Haddad <paulh16@gmail.com>
2018-10-25 07:22:17 -0400

    Subpalindrome
    * All tests pass in Python3

--- lessons/subpalindrome/subpalindrome.py
+++ lessons/subpalindrome/subpalindrome.py
@@ -1,21 +1,24 @@
 def longest_subpalindrome_slice(string):
+    "Return (i, j) such that text[i:j] is the longest palindrome in text."
     normalized_string = string.lower()
 
     longest_subpalindrome = (0,0)
     for i, starting_letter in enumerate(normalized_string):
+        print(f"Starting letter: {starting_letter}")
         for x, ending_letter in enumerate(normalized_string[i:]):
             substring = normalized_string[i:i+x+1]
             print(substring)
 
             if _palindrome(substring):
                 if len(substring) > (longest_subpalindrome[1] - longest_subpalindrome[0]):
-                    print(f"Longest Palindrom! {substring}")
+                    print(f"Longest Palindrome! {substring}")
                     longest_subpalindrome = (i, i+x+1)
 
     return longest_subpalindrome
 
 
 def _palindrome(string):
+    "Return a Boolean indicating whether the string is a palindrome."
     reversed_string = ''
     for letter in reversed(string):
         reversed_string += letter
--- lessons/subpalindrome/test_subpalindrome.py
+++ lessons/subpalindrome/test_subpalindrome.py
@@ -6,10 +6,10 @@ from subpalindrome import longest_subpalindrome_slice
 def test_longest_subpalindrome_slice_for_palindromes():
     L = longest_subpalindrome_slice
 
-    # assert L('racecar') == (0, 7)
-    # assert L('Racecar') == (0, 7)
-    # assert L('') == (0, 0)
-    # assert L('xxxxx') == (0, 5)
+    assert L('racecar') == (0, 7)
+    assert L('Racecar') == (0, 7)
+    assert L('') == (0, 0)
+    assert L('xxxxx') == (0, 5)
 
 
 def test_longest_subpalindrome_slice_for_subpalindromes():
@@ -19,3 +19,4 @@ def test_longest_subpalindrome_slice_for_subpalindromes():
     assert L('xxxabc') == (0, 3)
     assert L('Mad am I ma dam.') == (0, 15)
     assert L('Race carr') == (7, 9)
+    assert L('something rac e car going') == (8,21)
