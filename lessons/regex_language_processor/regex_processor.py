415e988d28484debb3409a6febd4166294619624
Paul Haddad <paulh16@gmail.com>
2018-11-02 07:51:14 -0400

    Regex Processor
    * Build out the #matchset function.

---
+++ lessons/regex_language_processor/regex_processor.py
@@ -0,0 +1,32 @@
+def matchset(pattern, text):
+    """
+    Match pattern at start of text; return a set of remainders of text.
+    """
+    null = frozenset()
+
+    op, x, y = components(pattern)
+    if 'lit' == op:
+        return set([text[len(x):]]) if text.startswith(x) else null
+    elif 'seq' == op:
+        return set(t2 for t1 in matchset(x, text) for t2 in matchset(y, t1))
+    elif 'alt' == op:
+        return matchset(x, text) | matchset(y, text) # union of these matched sets
+    elif 'dot' == op:
+        return set([text[1:]]) if text else null
+    elif 'oneof' == op:
+        return set([text[1:]]) if text.startswith(x) else null
+    elif 'eol' == op:
+        return set(['']) if text == '' else null
+    elif 'star' == op:
+        return (set([text]) |
+                set(t2 for t1 in matchset(x, text)
+                    for t2 in matchset(pattern, t1) if t1 != text))
+    else:
+        raise ValueError(f"unknown pattern: {pattern}")
+
+
+def components(pattern):
+    "Return the op, x, and y arguments; x and y are None if missing."
+    x = pattern[1] if len(pattern) > 1 else None
+    y = pattern[2] if len(pattern) > 2 else None
+    return pattern[0], x, y
---
+++ lessons/regex_language_processor/regex_processor_test.py
@@ -0,0 +1,33 @@
+import pytest
+
+
+from regex_processor import matchset
+
+
+def test_matchset():
+    assert matchset(('lit', 'abc'), 'abcdef') == set(['def'])
+    assert matchset(('seq', ('lit', 'hi '), ('lit', 'there ')), 'hi there nice to meet you') == set(['nice to meet you'])
+    assert matchset(('alt', ('lit', 'dog'), ('lit', 'cat')), 'dog and cat') == set([' and cat'])
+    assert matchset(('dot',), 'am i missing something?') == set(['m i missing something?'])
+    assert matchset(('oneof', 'a'), 'aabc123') == set(['abc123'])
+    assert matchset(('eol',),'') == set([''])
+    assert matchset(('eol',),'not end of line') == frozenset([])
+    assert matchset(('star', ('lit', 'hey')), 'heyhey!') == set(['!', 'heyhey!', 'hey!'])
+
+
+# def test_search():
+#     a,b,c = lit('a'), lit('b'), lit('c')
+#     abcstars = seq(star(a), seq(star(b), star(c)))
+#     dotstar = star(dot)
+#     assert search(lit('def'), 'abcdefg') == 'def'
+#     assert search(seq(lit('def'), eol), 'abcdef') == 'def'
+#     assert search(seq(lit('def'), eol), 'abcdefg') is None
+#     assert search(a, 'not the start') == 'a'
+#     assert match(a, 'not the start') is None
+#     assert match(abcstars, 'aaabbbccccccccdef') == 'aaabbbccccccccdef'
+#     assert match(abcstars, 'junk') == ''
+#     assert all(match(seq(abcstars, eol), s) == s for s in 'abc aaabbccc aaaabcccc'.split())
+#     r = seq(lit('ab'), seq(dotstar, seq(lit('aca'), seq(dotstar, seq(a, eol)))))
+#     assert all(search(r, s) is not None for s in 'abracadabra abacaa about-acacia-flora'.split())
+#     assert all(match(seq(c, seq(dotstar, b)), s) is not None for s in 'cab cob carob cb carbuncle'.split())
+#     assert not any(match(seq(c, seq(dot, b)), s) for s in 'crab cb across scab'.split())
