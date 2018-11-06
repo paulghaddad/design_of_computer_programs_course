a47be838979e35e21c01697a4c97db62becdf1f5
Paul Haddad <paulh16@gmail.com>
2018-11-06 08:07:26 -0500

    Regex Compiler
    * Build `alt`

--- lessons/regex_language_processor/regex_compiler.py
+++ lessons/regex_language_processor/regex_compiler.py
@@ -5,3 +5,6 @@ def lit(x): return lambda text: set([text[len(x):]]) if text.startswith(x) else 
 
 
 def seq(x, y): return lambda text: set().union(*map(y, x(text)))
+
+
+def alt(x, y): return lambda text: x(text) | y(text)
