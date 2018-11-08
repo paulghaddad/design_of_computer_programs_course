d83b3274e4f0813fd443050f9934db30034a49d4
Paul Haddad <paulh16@gmail.com>
2018-11-08 07:03:06 -0500

    Regex Compiler
    * Add `oneof`, `dot` and `eol`

--- lessons/regex_language_processor/regex_compiler.py
+++ lessons/regex_language_processor/regex_compiler.py
@@ -8,3 +8,12 @@ def seq(x, y): return lambda text: set().union(*map(y, x(text)))
 
 
 def alt(x, y): return lambda text: x(text) | y(text)
+
+
+def oneof(chars): return lambda text: set([text[1:]]) if (text and text[0] in chars) else null
+
+
+dot = lambda text: set([text[:1]]) if text else null
+
+
+eol = lambda text: set(['']) if text == '' else null
