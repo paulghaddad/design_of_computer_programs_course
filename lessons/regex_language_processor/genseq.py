b2ebeecb2ce37c9acd50b0b1ff3d83bdd3bad2b3
Paul Haddad <paulh16@gmail.com>
2018-11-19 08:21:10 -0500

    Regex Language Processor
    * Implement the decorator decorator.

--- lessons/regex_language_processor/genseq.py
+++ lessons/regex_language_processor/genseq.py
@@ -4,15 +4,24 @@ from functools import update_wrapper
 null = frozenset([])
 
 
+def decorator(d):
+    "Make function d a decorator: d wraps a function fn."
+    def _d(fn):
+        return update_wrapper(d(fn), fn)
+    update_wrapper(_d, d)
+    return _d
+
+
+@decorator
 def n_ary(f):
     """Given binary function f(x, y), return an n_ary function such
     that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x."""
     def n_ary_f(x, *args):
         return x if not args else f(x, n_ary_f(*args))
-    update_wrapper(n_ary_f, f)
     return n_ary_f
 
 
+@n_ary
 def seq(x, y): return ('seq', x, y)
 
 
@@ -38,7 +47,8 @@ def oneof(chars): return lambda Ns: set(chars) if 1 in Ns else null
 
 
 # no need to specify startx because no infinite recursion is possible
-# def seq(x, y): return lambda Ns: genseq(x, y, Ns)
+
+def seq(x, y): return lambda Ns: genseq(x, y, Ns)
 
 
 def opt(x): return alt(epsilon, x)
