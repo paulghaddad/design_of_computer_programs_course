8d87c955548e268451c46e0b132b615c951f1a03
Paul Haddad <paulh16@gmail.com>
2018-11-20 07:47:03 -0500

    Create a tools file to demonstrate `memo`, `countcalls` and `trace`.

--- lessons/regex_language_processor/genseq.py
+++ lessons/regex_language_processor/genseq.py
@@ -21,24 +21,6 @@ def n_ary(f):
     return n_ary_f
 
 
-@decorator
-def memo(f):
-    """Decorator that caches the return value for each call to f(args).
-    Then when called again with same args,  we can just look it up."""
-    cache = {}
-    def _f(*args):
-        try:
-            return cache[args]
-        except KeyError:
-            cache[args] = result = f(*args)
-            return result
-        except TypeError:
-            # some element of args can't be a dict key
-            return f(args)
-
-    return _f
-
-
 @n_ary
 def seq(x, y): return ('seq', x, y)
 
