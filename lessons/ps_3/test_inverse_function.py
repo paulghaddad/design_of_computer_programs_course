4e18c6e92e8604f0a3550b6a733d3c882b122789
Paul Haddad <paulh16@gmail.com>
2018-12-07 06:52:06 -0500

    Inverse Function
    * Add a second test for a longer running function

--- lessons/ps_3/test_inverse_function.py
+++ lessons/ps_3/test_inverse_function.py
@@ -7,7 +7,7 @@ from inverse_function import inverse
 def square(x): return x*x
 
 
-def test_inverse_function():
+def test_inverse_function_small_x():
     sqrt = inverse(square)
 
     t0 = time.perf_counter()
@@ -16,3 +16,14 @@ def test_inverse_function():
 
     logging.getLogger().info(f"The function took {t1-t0} seconds to perform")
     assert result == 10.0
+
+
+def test_inverse_function_large_x():
+    sqrt = inverse(square)
+
+    t0 = time.perf_counter()
+    result = sqrt(1000000000)
+    t1 = time.perf_counter()
+
+    logging.getLogger().info(f"The function took {t1-t0} seconds to perform")
+    assert result == 31622.7734375
