465e7bb05c75399de823300a57eb972bc6251af9
Paul Haddad <paulh16@gmail.com>
2018-12-11 07:00:24 -0500

    Find HTML Tags
    * Get first test passing

---
+++ lessons/ps_3/find_html_tags.py
@@ -0,0 +1,11 @@
+import re
+
+
+HTML_START_TAG_PATTERN = re.compile(r'<[a-z]\s*([a-z]+="[\w\.]+")*>')
+
+def findtags(text):
+    """
+    Return a list of all the html start tags in the text.
+    """
+    matches = re.finditer(HTML_START_TAG_PATTERN, text)
+    return [match.group() for match in matches]
---
+++ lessons/ps_3/test_find_html_tags.py
@@ -0,0 +1,34 @@
+import pytest
+
+
+from find_html_tags import findtags
+
+testtext1 = """
+My favorite website in the world is probably 
+<a href="www.udacity.com">Udacity</a>. If you want 
+that link to open in a <b>new tab</b> by default, you should
+write <a href="www.udacity.com"target="_blank">Udacity</a>
+instead!
+"""
+
+testtext2 = """
+Okay, so you passed the first test case. <let's see> how you 
+handle this one. Did you know that 2 < 3 should return True? 
+So should 3 > 2. But 2 > 3 is always False.
+"""
+
+testtext3 = """
+It's not common, but we can put a LOT of whitespace into 
+our HTML tags. For example, we can make something bold by
+doing <         b           > this <   /b    >, Though I 
+don't know why you would ever want to.
+"""
+
+
+def test_findtags():
+    assert findtags(testtext1) == ['<a href="www.udacity.com">',
+                                   '<b>',
+                                   '<a href="www.udacity.com"target="_blank">']
+
+    # assert findtags(testtext2) == []
+    # assert findtags(testtext3) == ['<         b           >']
