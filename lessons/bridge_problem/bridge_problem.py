352c6cd1d4c15c8506baddc7437c806eeba1e345
Paul Haddad <paulh16@gmail.com>
2018-12-18 07:21:23 -0500

    Refactor `path_states` and `path_actions` with list slicing.

--- lessons/bridge_problem/bridge_problem.py
+++ lessons/bridge_problem/bridge_problem.py
@@ -23,19 +23,9 @@ def bsuccessors(state):
 
 def path_states(path):
     "Return a list of states in this path."
-    states = []
-    for i, element in enumerate(path):
-        if i % 2 == 0:
-            states.append(element)
-
-    return states
+    return path[::2]
 
 
 def path_actions(path):
     "Return a list of actions in this path."
-    actions = []
-    for i, element in enumerate(path):
-        if i % 2 != 0:
-            actions.append(element)
-
-    return actions
+    return path[1::2]
