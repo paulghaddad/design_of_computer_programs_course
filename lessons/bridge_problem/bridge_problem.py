24996586a645e132e7b389bd7c3d0cf2e8d7e504
Paul Haddad <paulh16@gmail.com>
2018-12-20 07:41:26 -0500

    Update the bridge problem to return the one with least cost.
    * End of video 18.

--- lessons/bridge_problem/bridge_problem.py
+++ lessons/bridge_problem/bridge_problem.py
@@ -94,6 +94,50 @@ def bridge_problem(here):
     return []
 
 
+def bridge_problem2(here):
+    here = frozenset(here) | frozenset(['light'])
+    explored = set() # set of states we have visited
+    # State will be a (people-here, people-there) tuple
+    # e.g. ({1, 2, 5, 10, 'light'}, {})
+    frontier = [ [(here, frozenset())] ] # ordered list of paths we have blazed
+    while frontier:
+        path = frontier.pop(0)
+        here1, there1 = state1 = final_state(path)
+        if not here1 or (len(here1) == 1 and 'light' in here1):
+            return path
+        explored.add(state1)
+        pcost = path_cost(path)
+        for (state, action) in bsuccessors2(state1).items():
+            if state not in explored:
+                total_cost = pcost + bcost(action)
+                path2 = path + [(action, total_cost), state]
+                add_to_frontier(frontier, path2)
+    return Fail
+
+
+Fail = None
+
+
+def final_state(path): return path[-1]
+
+
+def add_to_frontier(frontier, path):
+    "Add path to frontier, replacing costlier path if there is one."
+    # (This could be done more efficiently.)
+    # Find if there is an old path to the final state of this path.
+    old = None
+    for i,p in enumerate(frontier):
+        if final_state(p) == final_state(path):
+            old = i
+            break
+    if old is not None and path_cost(frontier[old]) < path_cost(path):
+        return # Old path was better; do nothing
+    elif old is not None:
+        del frontier[old] # Old path was worse; delete it
+    # Now add the new path and resort
+    frontier.append(path)
+
+
 def elapsed_time(path):
     return path[-1][2]
 
