c4a87ce8b23cec2e9011c080a6da0e41338bc9c6
Paul Haddad <paulh16@gmail.com>
2018-12-21 08:36:33 -0500

    Finish out `csuccessors`.
    * End of video 22

--- lessons/missionaries_and_cannibals/test_missionaries_and_cannibals.py
+++ lessons/missionaries_and_cannibals/test_missionaries_and_cannibals.py
@@ -1,7 +1,26 @@
 import pytest
 
 
-from missionaries_and_cannibals import csuccessors
+from missionaries_and_cannibals import (
+    my_csuccessors,
+    csuccessors
+)
+
+
+def test_my_csuccessors():
+    assert my_csuccessors((2, 2, 1, 0, 0, 0)) == {(2, 1, 0, 0, 1, 1): 'C->',
+                                               (1, 2, 0, 1, 0, 1): 'M->',
+                                               (0, 2, 0, 2, 0, 1): 'MM->',
+                                               (1, 1, 0, 1, 1, 1): 'MC->',
+                                               (2, 0, 0, 0, 2, 1): 'CC->'}
+
+    assert my_csuccessors((1, 1, 0, 4, 3, 1)) == {(1, 2, 1, 4, 2, 0): '<-C',
+                                                (2, 1, 1, 3, 3, 0): '<-M',
+                                                (3, 1, 1, 2, 3, 0): '<-MM',
+                                                (1, 3, 1, 4, 1, 0): '<-CC',
+                                                (2, 2, 1, 3, 2, 0): '<-MC'}
+
+    assert my_csuccessors((1, 4, 1, 2, 2, 0)) == {}
 
 
 def test_csuccessors():
